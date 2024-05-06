d05e679c5a82f8a7b3f8eb018f336f41f5208d57
jmielc2 <jmielc2@uic.edu>
2024-05-06 14:16:58 -0500

    Returning to old version

--- Perspective-Divide/shaders/frag-shader.fs
+++ Perspective-Divide/shaders/frag-shader.fs
@@ -1,9 +1,9 @@
 #version 450
 
-in vec3 uv;
+in vec2 uv;
 
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1.0f, uv.x, uv.y, 1.0f);
+    fragColor = vec4(1.0, uv.x, uv.y, 1.0);
 }
--- Perspective-Divide/shaders/vert-shader.vs
+++ Perspective-Divide/shaders/vert-shader.vs
@@ -6,7 +6,7 @@ uniform mat4 uProjection;
 in vec3 aVertex;
 in vec2 aTexture;
 
-out vec3 uv;
+out vec2 uv;
 
 void main() {
 	gl_Position = uProjection * uModel * vec4(aVertex, 1.0);
